9b41648cfac50e528cf87ee0394138a6b9afc830
Andrei Osadchii <nc1xo099@gmail.com>
2022-05-29 16:38:53 +0400

    Add request and form withing it

--- src/main/request/Request.js
+++ src/main/request/Request.js
@@ -1,7 +1,33 @@
-import React, { Component } from "react";
+import React from "react";
+import Call from "../call/Call";
 
-export default class Request extends Component {
-  render() {
-    return <div>Request</div>;
-  }
+function Request() {
+  return (
+    <div className="request__container">
+      <div className="request__text-block">
+        <h2 className="request__title title">
+          Request a free cleaning quote today
+        </h2>
+        <p className="request__text">
+          In dignissim euismod pretium amet enim a eu nam ut urna accumsan
+          pellentesque lacus duis pharetra eutortor.
+        </p>
+        <Call />
+        <div className="separator_small"></div>
+        <h3 className="request__subtitle">Not convinced yet?</h3>
+        <p className="request__text">
+          Massa bibendum consectetur maurisid gravida purus, dolor dui amet
+          morbi non nunc urna purus diam.
+        </p>
+        <div className="request__button">
+          <a href="index.html" className="button_white">
+            Browse our packages
+          </a>
+        </div>
+      </div>
+      {/* TODO FORM */}
+    </div>
+  );
 }
+
+export default Request;
