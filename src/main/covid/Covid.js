0a19a3fdee4fe6efbe57a2ea648029148e3b71d0
Andrei Osadchii <nc1xo099@gmail.com>
2022-05-29 14:19:56 +0400

    rmk class component to functional comp

--- src/main/covid/Covid.js
+++ src/main/covid/Covid.js
@@ -1,35 +1,35 @@
-import React, { Component } from "react";
+import React from "react";
 import Call from "../call/Call";
 import covid from "../../assets/img/covid/covid.svg";
 
-export default class Covid extends Component {
-  render() {
-    return (
-      <div className="covid__container">
-        <div className="covid__decor">
-          <img src={covid} alt="covid-decor" />
+function Covid() {
+  return (
+    <div className="covid__container">
+      <div className="covid__decor">
+        <img src={covid} alt="covid-decor" />
+      </div>
+      <div className="covid__content">
+        <div className="covid__text">
+          <h3 className="covid__title-small">Covid-19 sanitization</h3>
+          <h2 className="covid__title title_white">
+            We follow guidelines to keep you safe from the COVID-19 virus
+          </h2>
+          <p className="covid__description">
+            Lobortis mattis odio leo eget mauris met aliquet semper molestie
+            sollicitudin congue massa mauris lectus.
+          </p>
         </div>
-        <div className="covid__content">
-          <div className="covid__text">
-            <h3 className="covid__title-small">Covid-19 sanitization</h3>
-            <h2 className="covid__title title_white">
-              We follow guidelines to keep you safe from the COVID-19 virus
-            </h2>
-            <p className="covid__description">
-              Lobortis mattis odio leo eget mauris met aliquet semper molestie
-              sollicitudin congue massa mauris lectus.
-            </p>
-          </div>
-          <div className="covid__action">
-            <a href="index.html" className="covid__button button">
-              Get a free quote
-            </a>
-            <div className="covid__call">
-              <Call modifier={"_white"} />
-            </div>
+        <div className="covid__action">
+          <a href="index.html" className="covid__button button">
+            Get a free quote
+          </a>
+          <div className="covid__call">
+            <Call modifier={"_white"} />
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+export default Covid;
