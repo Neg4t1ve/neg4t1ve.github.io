505ed6cb9bfc41e112d541148fd0888e744bd806
Andrei Osadchii <nc1xo099@gmail.com>
2022-05-29 14:18:23 +0400

    rmk class component to functional comp

--- src/main/video-block/videoBlock.js
+++ src/main/video-block/videoBlock.js
@@ -1,63 +1,55 @@
-import React, { Component } from "react";
+import React from "react";
 import clients from "../../assets/img/video-block/clients.svg";
 import done from "../../assets/img/video-block/done.svg";
 import employe from "../../assets/img/video-block/employe.svg";
 import placeholder from "../../assets/img/video-block/video-placeholder.svg";
 
-export default class VideoBlock extends Component {
-  render() {
-    return (
-      <div className="video-block__container">
-        <div className="video-block__header">
-          <h2 className="video-block__title title">
-            Our cleaning services have no comparison
-          </h2>
-          <p className="video-block__text">
-            Lobortis mattis odio leo eget mauris met aliquet semper molestie
-            sollicitudin congue massa mauris lectus vitae cras viverra gravida
-            sapien.
-          </p>
-        </div>
-        <ul className="vide-block__stats stats-list">
-          <li className="stats-list__column">
-            <img
-              src={clients}
-              alt="clients-icon"
-              className="stats-list__icon"
-            />
-            <div className="stats-list__text">
-              <h4 className="stats-list__title">clients</h4>
-              <p className="stats-list__count">3,480+</p>
-            </div>
-          </li>
-          <li className="stats-list__column">
-            <img src={done} alt="done-icon" className="stats-list__icon" />
-            <div className="stats-list__text">
-              <h4 className="stats-list__title">jobs done</h4>
-              <p className="stats-list__count">12,540+</p>
-            </div>
-          </li>
-          <li className="stats-list__column">
-            <img
-              src={employe}
-              alt="employe-icon"
-              className="stats-list__icon"
-            />
-            <div className="stats-list__text">
-              <h4 className="stats-list__title">Employees</h4>
-              <p className="stats-list__count">100+</p>
-            </div>
-          </li>
-        </ul>
-        <div className="video-block__video">
-          <img
-            src={placeholder}
-            alt="placeholder"
-            className="video-block__placeholder"
-          />
-        </div>
-        <div className="separator"></div>
+function VideoBlock() {
+  return (
+    <div className="video-block__container">
+      <div className="video-block__header">
+        <h2 className="video-block__title title">
+          Our cleaning services have no comparison
+        </h2>
+        <p className="video-block__text">
+          Lobortis mattis odio leo eget mauris met aliquet semper molestie
+          sollicitudin congue massa mauris lectus vitae cras viverra gravida
+          sapien.
+        </p>
       </div>
-    );
-  }
+      <ul className="vide-block__stats stats-list">
+        <li className="stats-list__column">
+          <img src={clients} alt="clients-icon" className="stats-list__icon" />
+          <div className="stats-list__text">
+            <h4 className="stats-list__title">clients</h4>
+            <p className="stats-list__count">3,480+</p>
+          </div>
+        </li>
+        <li className="stats-list__column">
+          <img src={done} alt="done-icon" className="stats-list__icon" />
+          <div className="stats-list__text">
+            <h4 className="stats-list__title">jobs done</h4>
+            <p className="stats-list__count">12,540+</p>
+          </div>
+        </li>
+        <li className="stats-list__column">
+          <img src={employe} alt="employe-icon" className="stats-list__icon" />
+          <div className="stats-list__text">
+            <h4 className="stats-list__title">Employees</h4>
+            <p className="stats-list__count">100+</p>
+          </div>
+        </li>
+      </ul>
+      <div className="video-block__video">
+        <img
+          src={placeholder}
+          alt="placeholder"
+          className="video-block__placeholder"
+        />
+      </div>
+      <div className="separator"></div>
+    </div>
+  );
 }
+
+export default VideoBlock;
